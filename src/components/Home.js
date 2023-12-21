3b50ceccdd2d85669645eb9d0471bd42b104b619
Mawuli Hormeku <hormekumawuli93@gmail.com>
2023-12-21 12:12:00 +0000

    edit the profile i the home.js page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,20 @@
 import React from "react";
+//import About from "./About";
+import Contact from "./Contact"
+import Project from "./Project"
 import image from "../Image/coding.jpg"; // Replace with the actual path to your image
 function Home() {
   return (
     <div >
       <div>
-        <h2>Welcome to Our Website</h2>
+        <h2>Welcome to hmawuli Profile</h2>
         <p>
           As a software engineer, I bring innovative solutions to the digital
-          world. Explore and discover the possibilities with cutting-edge
+          world.<br/> Explore and discover the possibilities with cutting-edge
           technology!
         </p>
-        
+          <Project/>
+          <Contact/>
         {/* Add more content as needed */}
       </div>
       <img
