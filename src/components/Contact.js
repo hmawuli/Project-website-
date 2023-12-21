d77a00b67e6508efbd0c1a51b91714006fa0a44c
Mawuli Hormeku <hormekumawuli93@gmail.com>
2023-12-21 12:17:26 +0000

    edit the contact.js contend

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,9 @@ function Contact(){
         <div>
       <p>
         Feel free to reach out if you have any questions, opportunities, or just
-        want to say hello!
+        want to say hello.
       </p>
-      <p>Email: hormekumawuli93@gmail.com</p>
+      <p>Email: hormekumawuli93@gmail.com</p><br/>
       <p>Phone: +233 245 831 131</p>
       {/* Add more contact information or a contact form as needed */}
     </div>
